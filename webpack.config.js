ff7b065ebc5ed9d73075e72102000d2861eb231b
Andries Smit <mr.andries.smit@gmail.com>
2017-03-17 15:14:12 +0100

    Add project setup

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require("webpack");
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: "./src/ListViewSwipe.ts",
+    output: {
+        path: path.resolve(__dirname, "dist/tmp"),
+        filename: "src/com/mendix/widget/listviewSwipe/ListViewSwipe.js",
+        libraryTarget: "umd"
+    },
+    resolve: {
+        extensions: [ ".ts", ".js", ".json" ]
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, use: "ts-loader" },
+            { test: /\.css$/, loader: ExtractTextPlugin.extract({
+                fallback: "style-loader",
+                use: "css-loader"
+            }) }
+        ]
+    },
+    devtool: "source-map",
+    externals: [ /^mxui\/|^mendix\/|^dojo\/|^dijit\// ],
+    plugins: [
+        new CopyWebpackPlugin([
+            { from: "src/**/*.js" },
+            { from: "src/**/*.xml" }
+        ], {
+            copyUnmodified: true
+        }),
+        new ExtractTextPlugin("./src/com/mendix/widget/listviewswipe/ui/ListViewSwipe.css"),
+        new webpack.LoaderOptionsPlugin({
+            debug: true
+        })
+    ]
+};
