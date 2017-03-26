d6b51ec566f71cfc24f4fb9c3c5f22641d55a65d
Andries Smit <mr.andries.smit@gmail.com>
2017-03-26 19:26:34 +0200

    Split onPan function

--- src/HammerSwipe.ts
+++ src/HammerSwipe.ts
@@ -1,5 +1,6 @@
 import * as Hammer from "hammerjs";
 import * as domStyle from "dojo/dom-style";
+import { Utils } from "./Utils";
 
 interface SwipeOptions {
     afterSwipeAction: {left: AfterSwipeAction, right: AfterSwipeAction};
@@ -28,7 +29,7 @@ class HammerSwipe {
     private containerClass: string;
     private hammer: HammerManager;
     private options: SwipeOptions;
-    private swipedOut = false;
+    private isSwiped = false;
     private isScrolling = false;
     private backElement: { right: HTMLElement | undefined, left: HTMLElement | undefined };
     private afterElement: { right: HTMLElement | undefined, left: HTMLElement | undefined };
@@ -37,12 +38,13 @@ class HammerSwipe {
     private borderLeft = 0;
     private swipeDirection: number;
     private thresholdCompensation = 0;
+    private thresholdAcceptSwipe = { left: 30, right: 30 };
     // Internal settings
-    readonly thresholdScrolling = 60; // Pixels.
-    readonly swipeAcceptThreshold = 30; // Percentage.
-    readonly removeItemDelay = 400; // Milliseconds
-    readonly moveThreshold = 25; // Pixels
-    readonly flickVelocity = 1.2; // Pixels per milliseconds
+    readonly delayRemoveItem = 400; // Milliseconds
+    readonly thresholdScroll = 60; // Pixels.
+    //readonly thresholdAcceptSwipe = 30; // Percentage. //TODO stick to button
+    readonly thresholdMove = 25; // Pixels
+    readonly thresholdVelocity = 1.2; // Pixels per milliseconds
 
     constructor(container: HTMLElement, options: SwipeOptions) {
         this.container = container;
@@ -54,15 +56,17 @@ class HammerSwipe {
         this.hammer = new Hammer.Manager(this.container);
         this.hammer.add(new Hammer.Pan({
             direction: this.swipeDirection,
-            threshold: this.moveThreshold
+            threshold: this.thresholdMove
         }));
-        this.hammer.on("panstart panmove panend pancancel", event => this.onPan(event));
+        this.hammer.on("panstart", event => this.onPanStart(event));
+        this.hammer.on("panmove", event => this.onPanMove(event));
+        this.hammer.on("panend pancancel", event => this.onPanEnd(event));
 
         this.setupPanes(options);
-        this.border.left = -this.containerSize + this.findButtonsPositions("left");
-        this.border.right = this.findButtonsPositions("right");
-        this.registerExtraEvents("left");
-        this.registerExtraEvents("right");
+        this.border.left = -this.containerSize + this.findButtonBorder("left");
+        this.border.right = this.findButtonBorder("right");
+        this.addHideTransitionEvent("left");
+        this.addHideTransitionEvent("right");
     }
 
     destroy() {
@@ -72,7 +76,7 @@ class HammerSwipe {
     private setupPanes(options: SwipeOptions) {
         // Foreground is the mx-dataview-content
         this.foreElement = this.container.firstChild.firstChild as HTMLElement;
-        this.addClass(this.foreElement, "swipe-foreground");
+        Utils.addClass(this.foreElement, "swipe-foreground");
 
         this.backElement = {
             left: this.findElement(options.backgroundName.left, "Swipe container left", "swipe-background"),
@@ -84,7 +88,7 @@ class HammerSwipe {
         };
     }
 
-    private findButtonsPositions(direction: Direction): number {
+    private findButtonBorder(direction: Direction): number {
         let border = 0;
         const backElement = this.backElement[direction];
         if (backElement && this.options.afterSwipeAction[direction] === "button") {
@@ -109,217 +113,182 @@ class HammerSwipe {
 
     private findElement(name: string, displayName: string, addClass?: string): HTMLElement | undefined {
         const element = name ? this.container.querySelector(`.mx-name-${name}`) as HTMLElement : undefined;
-        if (name && !element) {
-            throw new Error(`no '${displayName}' found named ${name}`);
-        }
-        if (element && addClass) {
-            this.addClass(element, addClass);
-        }
         if (element) {
+            if (addClass) {
+                Utils.addClass(element, addClass);
+            }
             // Move additional elements to become a sibling of the the foreground
             this.foreElement.parentElement.appendChild(element);
+        }
+        if (name && !element) {
+            throw new Error(`no '${displayName}' found named ${name}`);
         }
         return element;
     }
 
-    private resetElements(animate = true) {
-        this.removeClass(this.backElement.right, "hide");
-        this.removeClass(this.backElement.left, "hide");
-        this.removeClass(this.foreElement, "swiped-out");
-        this.removeClass(this.container, "will-accept-swipe");
-        this.show(0, undefined, animate);
-    }
-
-    private onPan(event: HammerInput) {
+    private onPanStart(event: HammerInput) {
         if (event.pointerType === "mouse") return;
-        if (this.swipedOut && event.type === "panstart") {
+        if (this.isSwiped) {
             this.resetElements();
             return;
         }
-        if (this.swipedOut && event.type === "panend") {
-            this.swipedOut = false;
-            return;
-        }
-        if (this.swipedOut) {
-            return;
-        }
-
-        if (event.type === "panstart") {
-            this.isScrolling = false;
-            this.thresholdCompensation = event.deltaX;
-        }
-        if (this.isScrolling) {
-            return;
-        }
-        const maximumPercentage = 100;
-        let currentPercentage = (maximumPercentage / this.containerSize) * (event.deltaX - this.thresholdCompensation);
-        if (this.swipeDirection === Hammer.DIRECTION_RIGHT && currentPercentage < 0) {
-            currentPercentage = 0;
-        }
-        if (this.swipeDirection === Hammer.DIRECTION_LEFT && currentPercentage > 0) {
-            currentPercentage = 0;
-        }
-        let animate = false;
-        const isScrolling = Math.abs(event.deltaY) > this.thresholdScrolling;
-        if (isScrolling) {
-            this.isScrolling = true;
-            this.show(0, undefined, true);
-            return;
-        }
-        if (Math.abs(currentPercentage) > this.swipeAcceptThreshold ) {
-            this.addClass(this.container, "will-accept-swipe");
-        } else {
-            this.removeClass(this.container, "will-accept-swipe");
-        }
-        const direction: Direction | undefined = currentPercentage < 0 ? "left" : currentPercentage > 0 ? "right" : undefined;
-        if ((event.type === "panend" || event.type === "pancancel") && direction) {
-            if ((Math.abs(currentPercentage) > this.swipeAcceptThreshold
-                || Math.abs(event.velocityX) > this.flickVelocity)
-                && event.type === "panend") {
-                this.swipeAnimation(direction);
-                this.afterSwipeAnimation(direction);
-                return;
-            }
-            currentPercentage = 0;
-            animate = true;
-        }
-        this.show(currentPercentage, direction, animate);
+        this.isScrolling = false;
+        this.thresholdCompensation = event.deltaX;
     }
 
-    private show(currentPercentage = 0, direction: Direction | undefined, animate?: boolean) {
-        const hundredPercent = 100;
-        const pos = (this.containerSize / hundredPercent) * currentPercentage;
+    private onPanMove(event: HammerInput) {
+        if (event.pointerType === "mouse") return;
+        if (this.isSwiped) return;
+        if (this.isScrolling) return;
 
-        this.updateBackground(direction);
+        const isScrolling = Math.abs(event.deltaY) > this.thresholdScroll;
+        if (isScrolling) {
+            this.isScrolling = true;
+            this.show(0, true);
+            return;
+        }
 
+        const percentage = (100 / this.containerSize) * (event.deltaX - this.thresholdCompensation);
+        this.show(percentage, false);
+    }
+
+    private onPanEnd(event: HammerInput) {
+        if (event.pointerType === "mouse") return;
+        if (this.isScrolling) return;
+
+        if (this.isSwiped) {
+            this.isSwiped = false;
+            return;
+        }
+
+        const percentage = (100 / this.containerSize) * (event.deltaX - this.thresholdCompensation);
+        const direction = percentage < 0 ? "left" : "right";
+        if ((Math.abs(percentage) > this.thresholdAcceptSwipe[direction]
+            || Math.abs(event.velocityX) > this.thresholdVelocity)
+            && event.type === "panend") {
+            this.swipeAnimation(direction);
+            this.afterSwipeAnimation(direction);
+            return;
+        } else {
+            this.show(0, true);
+        }
+    }
+
+    private resetElements(animate = true) {
+        Utils.removeClass(this.backElement.right, "hide");
+        Utils.removeClass(this.backElement.left, "hide");
+        Utils.removeClass(this.foreElement, "swiped-out");
+        Utils.removeClass(this.container, "will-accept-swipe");
+        Utils.addClass(this.afterElement.right, "hidden");
+        Utils.addClass(this.afterElement.left, "hidden");
+        this.show(0, animate);
+    }
+
+    private show(percentage = 0, animate?: boolean) {
         if (animate) {
-            this.addClass(this.container, "animate");
+            Utils.addClass(this.container, "animate");
         } else {
-            this.removeClass(this.container, "animate");
-            this.addClass(this.container, pos < 0 ? "swiping-left" : "swiping-right");
+            Utils.removeClass(this.container, "animate");
         }
 
+        const direction = percentage < 0 ? "left" : percentage > 0 ? "right" : undefined;
+        if (direction && Math.abs(percentage) > this.thresholdAcceptSwipe[direction] ) {
+            Utils.addClass(this.container, "will-accept-swipe");
+        } else {
+            Utils.removeClass(this.container, "will-accept-swipe");
+        }
         if (direction) {
+            this.updateBackground(direction);
             const oppositeDirection = direction === "left" ? "right" : "left";
-            this.addClass(this.container, `swiping-${direction}`);
-            this.removeClass(this.container, `swiping-${oppositeDirection}`);
+            Utils.addClass(this.container, `swiping-${direction}`);
+            Utils.removeClass(this.container, `swiping-${oppositeDirection}`);
         } else {
-            this.removeClass(this.container, "swiping-right");
-            this.removeClass(this.container, "swiping-left");
+            Utils.removeClass(this.container, "swiping-right");
+            Utils.removeClass(this.container, "swiping-left");
         }
 
+        const pos = (this.containerSize / 100) * percentage;
         domStyle.set(this.foreElement, {
-            opacity: this.options.transparentOnSwipe ? 1 - Math.abs(currentPercentage / hundredPercent) : 1,
+            opacity: this.options.transparentOnSwipe ? 1 - Math.abs(percentage / 100) : 1,
             transform: "translate3d(" + pos + "px, 0, 0)"
         });
     }
 
-    private updateBackground(direction: Direction | undefined) {
-        this.addClass(this.afterElement.right, "hidden");
-        this.addClass(this.afterElement.left, "hidden");
-        if (direction) {
+    private updateBackground(direction: Direction) {
+        if (this.backElement.right !== this.backElement.left) {
             const oppositeDirection = direction === "left" ? "right" : "left";
-            if (this.backElement.right !== this.backElement.left) {
-                this.addClass(this.backElement[oppositeDirection], "hidden");
-            }
-            this.removeClass(this.backElement[direction], "hidden");
+            Utils.addClass(this.backElement[oppositeDirection], "hidden");
         }
-    }
-
-    private addClass(element: HTMLElement | undefined, className: string) {
-        if (element) {
-            element.classList.add(className);
-        }
-    }
-
-    private removeClass(element: HTMLElement | undefined, className: string) {
-        if (element && element.classList) {
-            element.classList.remove(className);
-        }
+        Utils.removeClass(this.backElement[direction], "hidden");
     }
 
     private swipeAnimation(direction: Direction) {
-        let pos;
+        Utils.addClass(this.container, "animate");
+        let pos = direction === "left" ? -this.containerSize : this.containerSize;;
         if (this.options.afterSwipeAction[direction] === "button") {
             pos = this.border[direction];
-        } else {
-            pos = direction === "left" ? -this.containerSize : this.containerSize;
         }
-        this.addClass(this.container, "animate");
         domStyle.set(this.foreElement, { transform: "translate3d(" + pos + "px, 0, 0)" });
-        this.swipedOut = true;
         if (this.options.afterSwipeAction[direction] === "none"
             || this.options.afterSwipeAction[direction] === "button") {
             this.addRestoreEvent(this.options.parentElement);
         }
+
+        this.isSwiped = true;
     }
 
     private addRestoreEvent(element: HTMLElement) {
         const restore = (event: MouseEvent) => {
-            if (!this.inElement(element, event.target as HTMLElement, [ "mx-button", "mx-link", "clickable" ])) {
+            if (!Utils.inElement(element, event.target as HTMLElement, [ "mx-button", "mx-link", "clickable" ])) {
                 event.stopPropagation();
             }
             this.resetElements();
-            this.swipedOut = false;
+            this.isSwiped = false;
             element.removeEventListener("click", restore, true);
         };
         element.addEventListener("click", restore, true);
     }
 
-    private inElement(elementContainer: HTMLElement, element: HTMLElement, classNames: string[]): boolean {
-        let hasClass = false;
-        classNames.forEach(className => hasClass = hasClass || element.classList.contains(className));
-        if (hasClass) {
-            return true;
-        } else {
-            if (elementContainer !== element) {
-                return this.inElement(elementContainer, element.parentNode as HTMLElement, classNames);
-            } else {
-                return false;
-            }
-        }
-    }
-
     private afterSwipeAnimation(direction: Direction) {
         if (this.options.afterSwipeAction[direction] === "back") {
             this.resetElements(true);
-            this.swipedOut = false;
+            this.isSwiped = false;
             this.options.callback(this.container, direction);
         } else if (this.options.afterSwipeAction[direction] === "reset") {
             setTimeout(() => {
                 this.resetElements(false);
-                this.swipedOut = false;
+                this.isSwiped = false;
                 this.options.callback(this.container, direction);
             }, this.options.callbackDelay);
         } else if (this.options.afterSwipeAction[direction] === "hide") {
             const oppositeDirection = direction === "left" ? "right" : "left";
             if (this.afterElement.right !== this.afterElement.left) {
-                this.addClass(this.afterElement[oppositeDirection], "hidden");
+                Utils.addClass(this.afterElement[oppositeDirection], "hidden");
             }
-            this.removeClass(this.afterElement[direction], "hidden");
+            Utils.removeClass(this.afterElement[direction], "hidden");
             setTimeout(() => {
-                this.addClass(this.container, "animate");
+                Utils.addClass(this.container, "animate");
                 domStyle.set(this.container, { height: 0 });
 
                 setTimeout(() => {
-                    this.addClass(this.container, "hide");
-                    this.removeClass(this.container, "animate");
+                    Utils.addClass(this.container, "hide");
+                    Utils.removeClass(this.container, "animate");
                     this.options.callback(this.container, direction);
-                }, this.removeItemDelay);
+                }, this.delayRemoveItem);
             }, this.options.callbackDelay);
         } else if (this.options.afterSwipeAction[direction] === "none") {
-            this.addClass(this.foreElement, "swiped-out");
+            Utils.addClass(this.foreElement, "swiped-out");
         }
     }
 
-    private registerExtraEvents(direction: Direction) {
+    private addHideTransitionEvent(direction: Direction) {
         if (this.options.afterSwipeAction[direction] === "hide") {
             this.foreElement.addEventListener("transitionend", () => {
-                if (this.swipedOut) {
+                if (this.isSwiped) {
                     domStyle.set(this.container, {
                         height: this.container.offsetHeight + "px"
                     });
-                    this.addClass(this.backElement[direction], "hide");
+                    Utils.addClass(this.backElement[direction], "hide");
                 }
             });
         }
---
+++ src/Utils.ts
@@ -0,0 +1,29 @@
+export class Utils {
+
+    static inElement(elementContainer: HTMLElement, element: HTMLElement, classNames: string[]): boolean {
+        let hasClass = false;
+        classNames.forEach(className => hasClass = hasClass || element.classList.contains(className));
+        if (hasClass) {
+            return true;
+        } else {
+            if (elementContainer !== element) {
+                return this.inElement(elementContainer, element.parentNode as HTMLElement, classNames);
+            } else {
+                return false;
+            }
+        }
+    }
+
+    static addClass(element: HTMLElement | undefined, className: string) {
+        if (element) {
+            element.classList.add(className);
+        }
+    }
+
+    static removeClass(element: HTMLElement | undefined, className: string) {
+        if (element && element.classList) {
+            element.classList.remove(className);
+        }
+    }
+
+}
